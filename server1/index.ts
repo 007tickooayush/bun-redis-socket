0c3eefc095b82631cf57e9dbfbfcf08951ed0989
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-01-08 20:07:08 +0530

    setup express server

--- server1/index.ts
+++ server1/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import { createAdapter } from '@socket.io/redis-adapter';
+import express from 'express';
+import http from 'http';
+import { createClient } from 'redis';
+import { Server } from "socket.io";
+
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server)
+
+const PORT = process.env.PORT || 3002;
+
+
+app.get('/api', (req:express.Request, res:express.Response) => {
+    res.send({data:'req from server2'})
+//   res.send('<h1>Hello world</h1>');
+});
+
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+});
