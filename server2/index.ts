fcaaec461257afafbc62446b83eecf145afaec90
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-01-08 20:43:42 +0530

    event added for redis pub/sub

--- server2/index.ts
+++ server2/index.ts
@@ -14,24 +14,44 @@ const PORT = process.env.PORT || 3003;
 
 // redis pub/sub clients
 const pubClient = createClient({
-    url: 'redis://localhost:6379'
+    url: process.env.REDIS_URL || 'redis://localhost:6379'
 });
 const subClient = createClient();
 
 Promise.all([pubClient.connect(),subClient.connect()]).then(() => {
     console.log('Redis pub sub connected');
 }).then(() => {
-    // io.adapter(createAdapter(pubClient,subClient)).listen(PORT);
-
     io.adapter(createAdapter(pubClient,subClient));
     server.listen(PORT, () => {
-        console.log(`listening on *:${PORT}`);
+        console.log(`listening on => ${PORT}`);
     });
 }).catch((err) => {
     console.error('Error occured :>>', err);
 });
 
+subClient.subscribe('userAddedRedis',(message) => {
+    console.log(`message: `,JSON.parse(message));
+}).then(() => {
+    console.log('userAddedRedis subscribed');
+}).catch(err => {
+    console.log('err :>> ', err);
+});
+
+
+io.on('connection', async (socket) => {
+    console.log(`user connected socket: ${socket.id}`)
+
+    socket.on('addNew', async (data) => {
+        await pubClient.publish('userAddedRedis', JSON.stringify({...data, socket: socket.id}));
+        console.log('addNew :>> ', data);
+        socket.broadcast.emit('newAdded',data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Client disconnected ${socket.id}`);
+    });
+});
+
 app.get('/api', (req:express.Request, res:express.Response) => {
-    res.send({data:'req from server2'})
-//   res.send('<h1>Hello world</h1>');
+    res.send({data:'req from server2'}) 
 });
