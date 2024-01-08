d9e7505936786954736cb19f9c04d2d11bf617b1
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-01-08 20:38:00 +0530

    redis adapters setup for socketio

--- server1/index.ts
+++ server1/index.ts
@@ -12,12 +12,27 @@ const io = new Server(server)
 
 const PORT = process.env.PORT || 3002;
 
+// redis pub/sub clients
+const pubClient = createClient({
+    url: 'redis://localhost:6379'
+});
+const subClient = createClient();
+
+Promise.all([pubClient.connect(),subClient.connect()]).then(() => {
+    console.log('Redis pub sub connected');
+}).then(() => {
+    // io.adapter(createAdapter(pubClient,subClient)).listen(PORT);
+
+    io.adapter(createAdapter(pubClient,subClient));
+    server.listen(PORT, () => {
+        console.log(`listening on *:${PORT}`);
+    });
+}).catch((err) => {
+    console.error('Error occured :>>', err);
+});
+
 
 app.get('/api', (req:express.Request, res:express.Response) => {
     res.send({data:'req from server2'})
 //   res.send('<h1>Hello world</h1>');
-});
-
-server.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
 });
