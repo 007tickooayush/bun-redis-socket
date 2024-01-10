c1387a30a790149b8447e7d39e401aeb964a159a
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-01-10 20:08:35 +0530

    client1 client side project configurations

--- client1/src/context/socket.ts
+++ client1/src/context/socket.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'socket.io-client'
 
-const SOCKET_URL = 'http://localhost:3010';
+const SOCKET_URL = 'http://localhost:3010'; // server2 connection
 
 export const socket = connect(SOCKET_URL);
 export const SocketContext = React.createContext({socket});
